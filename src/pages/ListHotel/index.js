ca84b2d5734adc306b98d2617dbd2989ded817ac
rickyfriady <friadi.ricki@gmail.com>
2022-10-08 23:14:08 +0700

    Make hotel page and Search item

--- src/pages/ListHotel/index.js
+++ src/pages/ListHotel/index.js
@@ -1,10 +1,26 @@
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import { Header } from '../../components';
 
 function ListHotel() {
+  const location = useLocation();
+  const [destination] = useState(location.state.destination);
   return (
     <div>
       <Header tipe="list" />
-
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-5xl flex gap-5">
+          <div className="flex-1 p-2 sticky top-2 h-max">
+            <h1 className="text-lg mb-2">Search</h1>
+            <div className="flex flex-col gap-2 mb-2">
+              <label htmlFor="destination-input">
+                Destination
+                <input placeholder={destination} id="destination-input" type="text" />
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
