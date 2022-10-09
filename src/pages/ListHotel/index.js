82e83bdb4809563d1b294ea6f3c421600f5dc794
rickyfriady <friadi.ricki@gmail.com>
2022-10-09 15:37:01 +0700

    fix hotel list pages

--- src/pages/ListHotel/index.js
+++ src/pages/ListHotel/index.js
@@ -11,25 +11,25 @@ function ListHotel() {
   const [destination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
-  // const [options, setOptions] = useState(location.state.options);
+  const [options] = useState(location.state.options);
 
   return (
     <div>
       <Header tipe="list" />
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-5xl flex gap-5">
-          <div className="flex-1 p-2 sticky top-20 h-max bg-iconNav">
-            <h1 className="text-xl mb-2">Search</h1>
-            <div className="flex flex-col gap-2 mb-2">
-              <label htmlFor="destination-input">
+          <div className="flex-1 p-2 sticky top-20 h-max bg-iconNav text-left ">
+            <h1 className="text-xl mb-2 font-bold">Search</h1>
+            <div className="mb-2">
+              <label htmlFor="destination-input" className="text-sm flex flex-col gap-2 ">
                 Destination
-                <input placeholder={destination} id="destination-input" type="text" />
+                <input placeholder={destination} id="destination-input" type="text" className="h-7 border-none p-1" />
               </label>
             </div>
             <div className="flex flex-col gap-2 mb-2">
-              <label htmlFor="range-input">
+              <label htmlFor="range-input" className="text-sm">
                 Check-in Date
-                <span onClick={() => setOpenDate(!openDate)}>
+                <span onClick={() => setOpenDate(!openDate)} className="h-7 p-1 bg-white flex items-center cursor-pointer mb-1">
                   {`${format(
                     date[0].startDate,
                     'MM/dd/yyyy',
@@ -45,9 +45,60 @@ function ListHotel() {
                 )}
               </label>
             </div>
+            <div className="mb-2 flex flex-col gap-2">
+              <span>
+                Options
+              </span>
+              <div className="p-2">
+                <div className="flex justify-between mb-2 text-heroSec text-xs">
+                  <span className="lsOptionText">
+                    Min price
+                    {' '}
+                    <small>per night</small>
+                  </span>
+                  <input type="number" className="w-12" />
+                </div>
+                <div className="flex justify-between mb-2 text-heroSec text-xs">
+                  <span className="lsOptionText">
+                    Max price
+                    {' '}
+                    <small>per night</small>
+                  </span>
+                  <input type="number" className="w-12" />
+                </div>
+                <div className="flex justify-between mb-2 text-heroSec text-xs">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="w-12"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="flex justify-between mb-2 text-heroSec text-xs">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="w-12"
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="flex justify-between mb-2 text-heroSec text-xs">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="w-12"
+                    placeholder={options.room}
+                  />
+                </div>
+              </div>
+            </div>
+            <button type="button" className="p-2 bg-heroSec text-iconNav border-none w-full font-medium cursor-pointer">Search</button>
           </div>
 
-          <div className="flex-3">
+          <div className="flex-[3_1_0%]">
             <SearchItem />
             <SearchItem />
             <SearchItem />
